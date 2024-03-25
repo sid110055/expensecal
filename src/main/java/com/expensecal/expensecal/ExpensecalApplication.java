06719f52c6ce70db3175aa1be276d9a5c621d490
Siddhartha Chikkavarapu <siddu_chikkavarapu@Siddharthas-MacBook-Pro.local>
2024-03-25 08:51:52 -0500

    Intial commit

---
+++ src/main/java/com/expensecal/expensecal/ExpensecalApplication.java
@@ -0,0 +1,13 @@
+package com.expensecal.expensecal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensecalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensecalApplication.class, args);
+	}
+
+}
