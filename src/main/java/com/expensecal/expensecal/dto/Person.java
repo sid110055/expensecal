13655faecfeddb6d2abe40c996b1b32063818fa3
RaoPrathyusha <154286132+RaoPrathyusha@users.noreply.github.com>
2024-04-12 11:42:27 -0500

    Add files via upload

--- src/main/java/com/expensecal/expensecal/dto/Person.java
+++ src/main/java/com/expensecal/expensecal/dto/Person.java
@@ -1,63 +1,25 @@
 package com.expensecal.expensecal.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-import java.util.List;
+@Data
+    public class Person {
+        @NotNull(message = "id cannot be null")
+        @JsonProperty("id")
+        private String Id;
 
-public class Person {
-    private Long id;
-    private String name;
-    private String userName;
-    private String pwd;
-    private List<String> expenses;
+        @NotNull(message = "name cannot be null")
+        @JsonProperty("name")
+        private String Name;
 
-    // Constructors
-    public Person() {}
+        @NotNull(message = "user_name cannot be null")
+        @JsonProperty("user_name")
+        private String user_name;
 
-    public Person(Long id, String name, String userName, String pwd, List<String> expenses) {
-        this.id = id;
-        this.name = name;
-        this.userName = userName;
-        this.pwd = pwd;
-        this.expenses = expenses;
+        @NotNull(message = "pwd cannot be null")
+        @JsonProperty("pwd")
+        private String pwd;
+
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
-
-    public List<String> getExpenses() {
-        return expenses;
-    }
-
-    public void setExpenses(List<String> expenses) {
-        this.expenses = expenses;
-    }
-}
